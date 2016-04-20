fb89a93755c6fbcd4cf78c00569c9195c3248ece
jonssmall <jonssmall@gmail.com>
2016-04-20 19:42:36 -0400

    placeholder logic for creating random sequence

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+var sequence = [];
+
 var testButton = document.getElementById("test-button");
 var testFunction = function() {
-	console.log("Hello");
-}
-
+	var oneToFour = getRandomInt(1,4);
+	sequence.push(oneToFour);
+	console.log(sequence);
+};
 testButton.addEventListener("click", testFunction);
 
+
+
+
+//http://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
