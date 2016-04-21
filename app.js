b8385a3780efa71c7cd358f5dda8b580ed5c3ccd
jonssmall <jonssmall@gmail.com>
2016-04-20 20:41:44 -0400

    gratuitous use of IIFE inside for loop to programatically event bind buttons that won't exist anyway

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 var sequence = [];
 
+var step = 0;
+
 var testButton = document.getElementById("test-button");
 var testFunction = function() {
-	var oneToFour = getRandomInt(1,4);
-	sequence.push(oneToFour);
+	addStep();
 	console.log(sequence);
+	//console.log(step);
+	console.log(sequence[step]);
+	step++;
 };
-testButton.addEventListener("click", testFunction);
+testButton.addEventListener("click", function(){
+	testFunction();
+});
 
+var oneButton = document.getElementById("one-button");
+var twoButton = document.getElementById("two-button");
+var threeButton = document.getElementById("three-button");
+var fourButton = document.getElementById("four-button");
 
+var buttons = [oneButton, twoButton, threeButton, fourButton];
 
+var getInput = function(number) {
+	console.log(number);
+};
+
+for (var i = 1; i < 5; i++ ) {
+	var button = buttons[i - 1];
+	(function(num) {
+		button.addEventListener("click", function() {
+			getInput(num);
+		});
+	})(i);
+}
+
+var addStep = function() {
+	sequence.push(getRandomInt(1,4));
+};
 
 //http://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
-function getRandomInt(min, max) {
+var getRandomInt = function (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
