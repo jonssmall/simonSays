c4d6bd1603ce7917baa4db42dd491e1973aa9472
jonssmall <jonssmall@gmail.com>
2016-04-27 20:10:06 -0400

    basic logic of game complete

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 var sequence = [];
-addStep(); //game starts with 1 instruction
-console.log(sequence);
 
 var step = 0;
 
@@ -13,13 +11,17 @@ var buttons = [oneButton, twoButton, threeButton, fourButton];
 
 var compareInput = function(number) {
 	var currentStep = sequence[step];
-	if(number == currentStep) {
-		console.log("Good job!");
-		addStep();
-		step++;
-	} else {
-		console.log("Damn!");
+	if(number == currentStep) {		
+		if(step + 1 == sequence.length) { //Are you at the end of the sequence, winning the round?
+			step = 0; //restart player's sequence
+			addStep();	// Instructions grow by 1
+		} else {
+			step++; //If not, move to the next instruction
+		}		
+	} else {		
 		step = 0;
+		sequence = [];
+		addStep();
 	}
 	console.log(sequence);
 };
@@ -33,11 +35,16 @@ for (var i = 1; i < 5; i++ ) {
 	})(i);
 }
 
-var addStep = function() {
-	sequence.push(getRandomInt(1,4));
-};
-
 //http://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
 var getRandomInt = function (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+var addStep = function() {
+	sequence.push(getRandomInt(1,4));
+};
+
+addStep(); //game starts with 1 instruction
+console.log(sequence);
+
+//todo: game starting function 
