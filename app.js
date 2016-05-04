f3a2df5d57f2159ea4a59fc2bd0d9ab0212f2901
jonssmall <jonssmall@gmail.com>
2016-05-03 21:05:16 -0400

    lighting up instructions automatically

--- app.js
+++ app.js
@@ -17,13 +17,18 @@ var Button = function(domElementId, value, sound) {
     this.flashColor = colorHack[this.value];
     this.beep = new Audio(this.sound);
     this.domElement.addEventListener("click", function() {
-    	rootScope.domElement.setAttribute("style", "background-color: #" + rootScope.flashColor);
-    	setTimeout(function(){ 
-    		rootScope.domElement.setAttribute("style", ""); 
-    	}, 500);
-		rootScope.beep.play();
+    	rootScope.lightUp();
 		compareInput(rootScope.value);
 	});
+};
+
+Button.prototype.lightUp = function() {
+	var buttonDom = this.domElement;
+	buttonDom.setAttribute("style", "background-color: #" + this.flashColor);
+	setTimeout(function(){ 
+		buttonDom.setAttribute("style", ""); 
+	}, 500);
+	this.beep.play();
 };
 
 var oneButton = new Button("one-button", 1, "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
@@ -31,12 +36,23 @@ var twoButton = new Button("two-button", 2, "https://s3.amazonaws.com/freecodeca
 var threeButton = new Button("three-button", 3, "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
 var fourButton = new Button("four-button", 4, "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
 
+var buttons = [oneButton, twoButton, threeButton, fourButton];
+var lightUpInstructions = function(sequence) {
+	var clonedSequence = sequence.slice();
+	var instructionInterval = setInterval(function() {
+		var currentButton = buttons[clonedSequence.shift() - 1];// sequence is 1 to 4, array is 0 to 3
+		currentButton.lightUp();
+		if (!clonedSequence.length) clearInterval(instructionInterval);
+	}, 1000);
+};
+
 var compareInput = function(number) {
 	var currentStep = sequence[step];
 	if(number == currentStep) {		
 		if(step + 1 == sequence.length) { //Are you at the end of the sequence, winning the round?
 			step = 0; //restart player's sequence
 			addStep();	// Instructions grow by 1
+			lightUpInstructions(sequence);
 		} else {
 			step++; //If not, move to the next instruction
 		}		
@@ -59,8 +75,10 @@ var addStep = function() {
 
 var startGame = function() {
 	sequence = [];
+	step = 0;
 	addStep(); //game starts with 1 instruction
 	console.log(sequence);
+	lightUpInstructions(sequence);
 };
 
 document.getElementById("start-button").addEventListener("click", function() {
